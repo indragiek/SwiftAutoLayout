340a6c647d7e4a3a17e3dd9266d90feb576e8f15
noahd <noahd@users.noreply.github.com>
2014-06-24 12:37:50 +0200

    Add application and test targets for OSX

---
+++ SwiftAutoLayout/SwiftAutoLayout-OSX/AppDelegate.swift
@@ -0,0 +1,21 @@
+//  Copyright (c) 2014 Indragie Karunaratne. All rights reserved.
+//  Licensed under the MIT license, see LICENSE file for more info.
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
